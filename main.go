199fe16c262717d6f4c802bd0a8f4d5d34e63cd6
Sam Maton <sam.maton91@gmail.com>
2024-11-05 21:51:24 +0000

    added basic command loop

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name    string
+	desc    string
+	command func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:    "help",
+			desc:    "Displays all of the available commands and their descriptions.",
+			command: commandHelp,
+		},
+
+		"exit": {
+			name:    "exit",
+			desc:    "Exits the Pokedex",
+			command: commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Help text here...")
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to the Pokedex!")
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		command, exists := getCommands()[scanner.Text()]
+
+		if !exists {
+			fmt.Println("Command does not exist.")
+			continue
+		}
+
+		command.command()
+	}
 }
